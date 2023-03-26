eb8d69e1417175170fe3f3956b37f2b4dcb6eb57
Aayushya Lakkadwala <TheGamingGod85@users.noreply.github.com>
2023-03-26 21:46:02 +0530

    this is really final

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Mulltimedia Editor", "Developer", "Designer", "Freelancer"],
+        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer", "Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer"],
+        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer", "Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
