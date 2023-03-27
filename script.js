5099d188f77a221786d5418276560cdf26d55b5a
Aayushya Lakkadwala <TheGamingGod85@users.noreply.github.com>
2023-03-27 19:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer", "Gamer"],
+        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer", "Gamer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer", "Gamer"],
+        strings: ["Multimedia Editor", "Developer", "Designer", "Freelancer", "Gamer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
